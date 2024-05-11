839e31e428d13c05ed260248ae821c2c24671829
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-05-11 00:44:55 +0000

    Chore: create points entity

---
+++ src/modules/points/entities/points.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+import { BaseEntity } from '@/common/entities/base.entity';
+import { LineEntity } from '@/modules/lines/entities/lines.entity';
+import { RythmEntity } from '@/modules/rythms/entities/rythtms.entity';
+
+@Entity('tb_pontos')
+export class PointsEntity extends BaseEntity {
+  /** Columns */
+  @Column()
+  letra: string;
+
+  @Column()
+  audio_url?: string;
+
+  /** Joins */
+
+  @ManyToOne(() => LineEntity, {
+    eager: true,
+    nullable: false,
+  })
+  @JoinColumn({
+    name: 'linha',
+    referencedColumnName: 'id',
+  })
+  linha: number;
+
+  @ManyToOne(() => RythmEntity, {
+    eager: true,
+    nullable: false,
+  })
+  ritmo: number;
+
+  /** Methods */
+}
